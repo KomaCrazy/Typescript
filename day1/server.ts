6e5d4bd7c958741299cff6bb8ba907a47cfe55eb
koma <zxloveninek90@gmail.com>
2024-05-22 00:10:56 +0700

    no message

--- day1/server.ts
+++ day1/server.ts
@@ -1,33 +1,46 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { userRoutes } from './routes/user.routes';
 import {ResponseHandler} from './services/service'
-
+import multer from 'multer';
 
 const app = express();
+
 const port = 3000;
+const upload = multer();
+app.use(upload.none());
+
+
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   console.log("========");
-  console.log(req.query);
+  // console.log(req.query);
+   const data = req.body.box; // รับข้อมูลจาก form-data
+    console.log(data);
   console.log("========");
   next();
 });
 
-app.use('/user', userRoutes);
-
 app.use((req: Request, res: Response, next: NextFunction) => {
 	const url:string = req.url as string 
-	const obj = {
-		error:""
-	}
-	// res.status(404).json(obj);
-	ResponseHandler.sendError(res, `Not Found >> ${url}`, 404);
+
+
+	ResponseHandler.sendError(res,"not_found", 404);
 });
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	const url:string = req.url as string   
   console.error(err.stack);  
-  res.status(500).json({ error: 'Something went wrong' });
+  ResponseHandler.sendError(res, `Something went wrong >> ${url}`, 500);
 });
+
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+
+});
+
+app.use('/user', userRoutes);
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
--- day1/services/service.ts
+++ day1/services/service.ts
@@ -1,17 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
+const LANG = require('../localization/server.json')
 
 interface RequiredResponse {
   status?: number;
+  msg?: string;
   error?: string;
   data?: any;
 }
 
 class ResponseHandler {
-  static sendResponse(res: Response, { status = 200, error = "", data = [] }: RequiredResponse): void {
+  static sendResponse(res: Response, { status = 200, msg="", error = "", data = [] }: RequiredResponse): void {
+    console.error("LANG",LANG)
+    LANG.error
     const response = {
+      msg,
       error,
       data
     };
+
+
     res.status(status).json(response);
   }
 
